f259eed49d3e6507281f68533bf895fec359e89b
HarperJr <doctorHarperjr@gmail.com>
2018-12-10 23:53:28 +0300

    Formatter for string

--- Application.cs
+++ Application.cs
@@ -3,7 +3,13 @@ namespace App {
 	public class Application {
 
 		public static void Main(string[] args) {
-			Console.WriteLine("Hello Git!");
+			Console.WriteLine(Formatter.Format("Hello world"));
+		}
+	}
+
+	public class Formatter {
+		public static string Format(string s) {
+			return Global.Locale.Formatter.Format(new Locale("en"), "UTF-8", s);
 		}
 	}
 }
